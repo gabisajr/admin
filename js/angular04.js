03e9dc8ac73027f65e246668a895a86a97768623
duffiye <duffiye@gmail.com>
2013-05-07 22:28:07 +0800

    modify angularjs04.js

--- js/angular04.js
+++ js/angular04.js
@@ -1,6 +1,12 @@
 function MyCtrl($scope) {
   $scope.action = function() {
+
+
     $scope.name = 'OK';
+
+    //ajax ,  return data 
+    // if data true, forword to the special url
+    // else in the current page
   }
 
   $scope.name = 'World';
